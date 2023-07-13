8eb6f38087f55c7b7d37c51937d43409ed075bb3
mustafa <abou08923@gmail.com>
2023-07-13 16:47:40 +0200

    division works

--- calculator.py
+++ calculator.py
@@ -1,17 +1,17 @@
 on = 1
-question = input("what do you want to do:\n1.multiplication\n2.division\n3.addition\n4.subtraction\n5.exit \n>")
+question = input("what do you want to do:\n1.multiplication\n2.division\n3.addition\n4.subtraction\n5.exit \n> ")
 def function(question):
-    if question == "1":
+    if question == "2":
         return "division"
-    elif question == "2":
+    elif question == "1":
         return "multiplication"
     else:
         return "Invalid option silly"
 value = function(question)
 print(function(question))
 while on == 1:
-    if value == "1":
-        answer1 = int(input("enter your first number"))
-        answer2 = int(input("enter your second number"))
+    if value == "division":
+        answer1 = int(input("enter your first number\n> "))
+        answer2 = int(input("enter your second number\n> "))
         print(answer1 / answer2)
         on = 0
